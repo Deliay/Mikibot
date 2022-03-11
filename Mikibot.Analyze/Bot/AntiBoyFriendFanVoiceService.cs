1b28731fd0b185d30384d90de12004f37568f45b
longxuan <admin@remiliascarlet.com>
2022-03-11 12:36:55 +0800

    update subject match

--- Mikibot.Analyze/Bot/AntiBoyFriendFanVoiceService.cs
+++ Mikibot.Analyze/Bot/AntiBoyFriendFanVoiceService.cs
@@ -76,7 +76,7 @@ namespace Mikibot.Analyze.Bot
                 {
                     if (rawMsg is PlainMessage plain)
                     {
-                        if (plain.Text.Contains('弥') && Regex.IsMatch(plain.Text, "女朋友|女友|结婚|男友|恋爱|老婆|二胎|三胎|孩子名字|想我|好喜欢你"))
+                        if (Regex.IsMatch(plain.Text, "弥|mxmk|毛线毛裤") && Regex.IsMatch(plain.Text, "女朋友|女友|结婚|男友|恋爱|老婆|二胎|三胎|孩子名字|想我|好喜欢你|🤤|😍|🥰|我的弥"))
                         {
                             Logger.LogInformation("检测到男友粉 {}({}) 发言：{}", msg.Sender.Name, msg.Sender.Id, plain.Text);
                             await MiraiService.SendMessageToGroup(msg.Sender.Group, token, notYourGrilFriend);
