4b34867049f7f5e474ef539e95b110c56ba7d20d
longxuan <admin@remiliascarlet.com>
2023-03-07 19:44:27 +0800

    enhance prompt

--- Mikibot.Analyze/Bot/AiImageGenerationService.cs
+++ Mikibot.Analyze/Bot/AiImageGenerationService.cs
@@ -355,7 +355,7 @@ namespace Mikibot.Analyze.Bot
             { "真", "yellow eyes, red hair, small breast, demon girl, demon tail, demon wings, " },
             { "悠", "(light blue eyes), black hair ribbon, silver hair, blue streaked hair, " },
             { "侑", "(white pink hair), (blue streaked hair), (cat_ear_headphone), <lora:Kiyuu_:0.3>, " },
-            { "炉", "yellow eyes, (pink to cyan gradient hair), ahoge, (beret hat), underwater, " },
+            { "炉", "yellow eyes, (pink to cyan gradient hair), ahoge, (hat), " },
         };
 
         private static readonly MessageChain helpMsg = new MessageChainBuilder()
