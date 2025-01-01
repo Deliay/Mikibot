d18d75e84dbaf9e0c9badf7b417f1da9f51af41a
ZeroAsh <admin@remiliascarlet.com>
2025-01-01 13:03:39 +0800

    feat: permission

--- Mikibot.Analyze/Generic/PermissionService.cs
+++ Mikibot.Analyze/Generic/PermissionService.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Mikibot.Database;
 using Mikibot.Database.Model;
 
 namespace Mikibot.Analyze.Generic;
 
-public class PermissionService(MikibotDatabaseContext db)
+public class PermissionService(MikibotDatabaseContext db, ILogger<PermissionService> logger)
 {
     public async ValueTask<bool> HasPermission(string role, string userId, string action, CancellationToken cancellationToken = default)
     {
-
-        return await db.Permissions.AnyAsync(
+        var result = await db.Permissions.AnyAsync(
             x => x.Role == role && x.UserId == userId && x.Action == action, cancellationToken);
+        
+        logger.LogInformation("Permission {}:{} action:{}, result: {}", role, userId, action, result);
+        return result;
     }
     
     public ValueTask<bool> IsBotOperator(string userId, CancellationToken cancellationToken = default)
