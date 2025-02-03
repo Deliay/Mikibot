b4cb39490a1f763e56bd2df26afdb67be739ced5
ZeroAsh <admin@remiliascarlet.com>
2025-02-03 17:08:45 +0800

    refactor chatbot and add ollama support

--- Mikibot.Analyze/Service/Ai/OllamaChatService.cs
+++ Mikibot.Analyze/Service/Ai/OllamaChatService.cs
@@ -27,7 +27,6 @@ public class OllamaChatService : IBotChatService
         var result = await ollamaClient.CompleteAsync(chats, new ChatOptions()
         {
             Temperature = chat.temperature,
-            ResponseFormat = ChatResponseFormat.Json,
         }, cancellationToken);
 
         var content = result.Message.Text;
