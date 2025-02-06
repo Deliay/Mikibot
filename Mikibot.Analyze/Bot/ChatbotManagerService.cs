4080816dad9a835d0089b0a00f3135361971425b
ZeroAsh <admin@remiliascarlet.com>
2025-02-06 15:30:10 +0800

    enhance: add new command to change LLM backend API when running

---
+++ Mikibot.Analyze/Bot/ChatbotManagerService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using Mikibot.Analyze.Generic;
+using Mikibot.Analyze.MiraiHttp;
+using Mikibot.Analyze.Service.Ai;
+using Mirai.Net.Data.Messages.Concretes;
+using Mirai.Net.Data.Messages.Receivers;
+
+namespace Mikibot.Analyze.Bot;
+
+public class ChatbotManagerService(
+    IMiraiService miraiService,
+    ILogger<ChatbotManagerService> logger,
+    PermissionService permissionService,
+    ChatbotSwitchService chatbotSwitchService)
+    : MiraiGroupMessageProcessor<ChatbotManagerService>(miraiService, logger)
+{
+
+    private const string VendorCommand = "/chatbot_vendor";
+    protected override async ValueTask Process(GroupMessageReceiver message, CancellationToken token = default)
+    {
+        foreach (var plainMessage in message.MessageChain.OfType<PlainMessage>())
+        {
+            var msg = plainMessage.Text.Trim();
+            if (!msg.StartsWith(VendorCommand)) return;
+
+            if (!await permissionService.IsBotOperator(message.Sender.Id, token)) return;
+            
+            var vendor = msg[VendorCommand.Length..];
+
+            if (!chatbotSwitchService.UpdateChatbot(vendor)) continue;
+            
+            await MiraiService.SendMessageToSomeGroup([message.GroupId], token,
+                new PlainMessage($"机器人后端成功更换为 {vendor}"));
+
+            return;
+        }
+        
+        
+    }
+}
---
+++ Mikibot.Analyze/Service/Ai/ChatbotSwitchService.cs
@@ -0,0 +1,26 @@
+﻿namespace Mikibot.Analyze.Service.Ai;
+
+public class ChatbotSwitchService
+{
+    
+    private readonly Dictionary<string, IBotChatService> chatbotMap;
+
+    public ChatbotSwitchService(IEnumerable<IBotChatService> chatbots)
+    {
+        chatbotMap = chatbots.ToDictionary(x => x.Id, x => x);
+        
+        Chatbot = chatbotMap.Values.FirstOrDefault()
+            ?? throw new ArgumentNullException(nameof(chatbots));
+    }
+
+    public IBotChatService Chatbot { get; private set; }
+
+
+    public bool UpdateChatbot(string id)
+    {
+        if (!chatbotMap.TryGetValue(id, out var botChatService)) return false;
+        
+        Chatbot = botChatService;
+        return true;
+    }
+}
