6cba56f2ca5aa6c49ee3b60046ebb7dbfad7fee1
ZeroAsh <admin@remiliascarlet.com>
2025-03-09 22:12:38 +0800

    add pixelized support

---
+++ Mikibot.Analyze/Bot/ImageProcessorService.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Extensions.Logging;
+using Mikibot.Analyze.Bot.Images;
+using Mikibot.Analyze.Generic;
+using Mikibot.Analyze.MiraiHttp;
+using Mirai.Net.Data.Messages;
+using Mirai.Net.Data.Messages.Concretes;
+using Mirai.Net.Data.Messages.Receivers;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Gif;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+
+namespace Mikibot.Analyze.Bot;
+
+public class ImageProcessorService(IMiraiService miraiService, ILogger<ImageProcessorService> logger)
+    : MiraiGroupMessageProcessor<ImageProcessorService>(miraiService, logger)
+{
+    static ImageProcessorService()
+    {
+        Configuration.Default.ImageFormatsManager.AddImageFormat(PngFormat.Instance);
+        Configuration.Default.ImageFormatsManager.AddImageFormat(JpegFormat.Instance);
+        Configuration.Default.ImageFormatsManager.AddImageFormat(GifFormat.Instance);
+    }
+
+    private HttpClient httpClient = new();
+
+    private static Func<(int, Image), (int, Image)> WrapStreamProcessor(Func<ReadOnlySpan<byte>, ReadOnlySpan<byte>> processor)
+    {
+        return frame =>
+        {
+            var (index, img) = frame;
+            using var frameImage = img;
+            using var before = new MemoryStream();
+            frameImage.SaveAsPng(before);
+
+            before.Position = 0;
+            var after = processor(before.ToArray());
+            return (index, Image.Load(after));
+        };
+    }
+
+
+    private static Image ProcessMultipleFrameImage(Image image, Func<(int, Image), (int, Image)> frameProcessor)
+    {
+        var proceedImages = Enumerable.Range(0, image.Frames.Count)
+            .Select(i => (i, image.Frames.CloneFrame(i)))
+            .AsParallel()
+            .Select(frameProcessor)
+            .OrderBy(f => f.Item1)
+            .ToList();
+        var templateFrame = proceedImages[0].Item2.Frames.CloneFrame(0);
+    
+        foreach (var (index, proceedImage) in proceedImages[1..])
+        {
+            templateFrame.Frames.InsertFrame(index, proceedImage.Frames.RootFrame);
+        }
+
+        return templateFrame;
+    }
+
+    private static readonly GifEncoder GifEncoder = new();
+    private static readonly PngEncoder PngEncoder = new();
+    
+    private static string BuildDataUri(string mimeType, string base64Data)
+    {
+        return $"data:{mimeType};base64,{base64Data}";
+    }
+
+    private static (IImageEncoder, Image, string) ProcessImage(Image image,
+        Func<(int, Image), (int, Image)> frameProcessor)
+    {
+        if (image.Frames.Count > 1)
+        {
+            return (new GifEncoder(), ProcessMultipleFrameImage(image, frameProcessor), "image/gif");
+        }
+        else
+        {
+            return (new PngEncoder(), frameProcessor((0, image)).Item2, "image/png");
+        }
+    }
+
+    private static async ValueTask<string> ProcessImageFromStreamToDataUri(Stream stream,
+        Func<(int, Image), (int, Image)> frameProcessor,
+        CancellationToken cancellationToken)
+    {
+        using var image = await Image.LoadAsync(stream, cancellationToken);
+        var (encoder, newImage, mimeType) = ProcessImage(image, frameProcessor);
+        using var afterImage = newImage;
+        await using var afterStream = new MemoryStream();
+        await newImage.SaveAsync(afterStream, encoder, cancellationToken);
+        afterStream.Position = 0;
+        var base64String = Convert.ToBase64String(afterStream.ToArray());
+
+        return BuildDataUri(mimeType, base64String);
+    }
+    
+    private async Task<ImageMessage> ProcessPixel(ImageMessage message, CancellationToken cancellationToken = default)
+    {
+        await using var stream = await httpClient.GetStreamAsync(message.Url, cancellationToken);
+        var processor = WrapStreamProcessor(PixelProcessor.Process);
+        var data = await ProcessImageFromStreamToDataUri(stream, processor, cancellationToken);
+        return new ImageMessage() { Base64 = data };
+    }
+
+    private Func<ImageMessage, CancellationToken, Task<ImageMessage>>? GetProcessor(string command)
+    {
+        if (command.StartsWith("/像素化")) return ProcessPixel;
+        else return null;
+    }
+    
+    protected override async ValueTask Process(GroupMessageReceiver message, CancellationToken token = default)
+    {
+        var msg = message.MessageChain.GetPlainMessage();
+        
+        var processor = GetProcessor(msg);
+        if (processor is null) return;
+
+        IEnumerable<QuoteMessage?> quoteMessages = [message.MessageChain.GetQuoteMessage()];
+
+        var imageMessages = message.MessageChain
+            .Concat(quoteMessages.Where(q => q is not null).SelectMany(q => q!.Origin))
+            .OfType<ImageMessage>().ToList();
+        
+        if (imageMessages.Count is 0 or > 10) return;
+
+        var processTasks = imageMessages.Select(imageMessage => processor(imageMessage, token));
+        
+        MessageBase[] result = await Task.WhenAll(processTasks);
+        
+        await MiraiService.SendMessageToSomeGroup([message.Sender.Group.Id], token, result);
+    }
+}
---
+++ Mikibot.Analyze/Bot/Images/PixelProcessor.cs
@@ -0,0 +1,51 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.XImgProc;
+
+namespace Mikibot.Analyze.Bot.Images;
+
+public static class PixelProcessor
+{
+    public static ReadOnlySpan<byte> Process(ReadOnlySpan<byte> input)
+    {
+        using var t = new ResourcesTracker();
+        var srcImage = t.T(Cv2.ImDecode(input, ImreadModes.Color));
+        
+        const double maxLenght = 512D;
+        var scaleSize = new Size(srcImage.Width, srcImage.Height);
+        if (scaleSize.Width > 1000) scaleSize = new Size(maxLenght, scaleSize.Height * (maxLenght / scaleSize.Width)); 
+        if (scaleSize.Height > 1000) scaleSize = new Size(scaleSize.Width * (maxLenght / scaleSize.Height), maxLenght); 
+
+        using var image = t.T(srcImage.Resize(scaleSize, interpolation: InterpolationFlags.Cubic));
+
+        using var edges = t.T(image.Canny(128, 200));
+        using var kernel = t.T(Cv2.GetStructuringElement(MorphShapes.Rect, new Size(2, 2)));
+        using var dilatedEdges = t.T(edges.Dilate(kernel, iterations: 2));
+
+        using var borderMask = t.T(edges.CvtColor(ColorConversionCodes.GRAY2BGR));
+        using var originalColorEdges = t.T(image.BitwiseAnd(borderMask));
+        using var darkerEdges = t.T(originalColorEdges * 0.5);
+
+        darkerEdges.ToMat().CopyTo(image, edges);
+
+        using var algorithm = t.T(SuperpixelSLIC.Create(image, regionSize: 5));
+
+        algorithm.Iterate(10);
+        algorithm.EnforceLabelConnectivity();
+
+        var labelCount = algorithm.GetNumberOfSuperpixels();
+
+        using var pixelImage = t.T(t.T(Mat.Zeros(image.Size(), image.Type())).ToMat());
+
+        using var labels = t.NewMat();
+        algorithm.GetLabels(labels);
+
+        Enumerable.Range(0, labelCount).AsParallel().ForAll(label =>
+        {
+            using var mask = new Mat();
+            Cv2.Compare(labels, label, mask, CmpType.EQ);
+            pixelImage[Cv2.BoundingRect(mask)].SetTo(Cv2.Mean(image, mask));
+        });
+
+        return pixelImage.ImEncode();
+    }
+}
