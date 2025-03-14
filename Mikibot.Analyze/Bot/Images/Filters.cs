b93e06060ba10041a92dcbf27a05460dd08566ea
ZeroAsh <admin@remiliascarlet.com>
2025-03-15 05:03:09 +0800

    enhance: new sliding command and frame delay command

--- Mikibot.Analyze/Bot/Images/Filters.cs
+++ Mikibot.Analyze/Bot/Images/Filters.cs
@@ -77,7 +77,7 @@ public static class Filters
 
             return image.ExtractFrames()
                 .FrameDelay(TimeSpan.FromMilliseconds(frameDelay))
-                .AutoComposeAsync(token);
+                .AutoComposeAsync(frameDelay / 10, token);
         });
     }
 }
