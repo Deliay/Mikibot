2b7f3e4d93bd78e4b168a1c805a64271bd42c864
ZeroAsh <admin@remiliascarlet.com>
2025-03-15 18:38:22 +0800

    enhance: add helper for meme processors

--- Mikibot.Analyze/Bot/FoodDice/FoodDiceService.cs
+++ Mikibot.Analyze/Bot/FoodDice/FoodDiceService.cs
@@ -24,12 +24,13 @@ public class FoodDiceService(IQqService qqService, ILogger<FoodDiceService> logg
     private const string 宵夜 = "宵夜";
     private const string 下午茶 = "下午茶";
 
-    private async ValueTask<RandomFood> RollRandomFood(string category, CancellationToken token)
+    private async ValueTask<RandomFood[]> RollRandomFood(string category, int count, CancellationToken token)
     {
         return await db.RandomFoods
             .OrderBy(c => EF.Functions.Random())
             .Where(c => c.Category == category)
-            .FirstOrDefaultAsync(token) ?? Wind;
+            .Take(count)
+            .ToArrayAsync(token);
     }
     
     protected override async ValueTask Process(GroupMessageReceiver message, CancellationToken token = default)
@@ -42,19 +43,20 @@ public class FoodDiceService(IQqService qqService, ILogger<FoodDiceService> logg
         var msg = msgObj.Text[2..];
 
         string category;
-        if (msg.StartsWith("吃什么")) category = 食物;
+        int count = 2;
+        if (msg.StartsWith("吃什么") || msg.StartsWith("吃啥")) (category, count) = (食物, 3);
         else if (msg.StartsWith("小酌什么")) category = 酒;
         else if (msg.StartsWith("喝什么")) category = 饮品;
-        else if (msg.StartsWith("早上吃什么") || msg.StartsWith("早餐吃什么")) category = 早餐;
-        else if (msg.StartsWith("中午吃什么") || msg.StartsWith("午餐吃什么")) category = 午餐;
-        else if (msg.StartsWith("下午吃什么") || msg.StartsWith("下午茶吃什么")) category = 下午茶;
-        else if (msg.StartsWith("晚上吃什么") || msg.StartsWith("晚餐吃什么")) category = 晚餐;
-        else if (msg.StartsWith("半夜吃什么") || msg.StartsWith("宵夜吃什么")) category = 宵夜;
+        else if (msg.StartsWith("早上吃什么") || msg.StartsWith("早餐吃什么") || msg.StartsWith("早餐吃啥")) (category, count) = (早餐, 2);
+        else if (msg.StartsWith("中午吃什么") || msg.StartsWith("午餐吃什么") || msg.StartsWith("午餐吃啥")) (category, count) = (午餐, 3);
+        else if (msg.StartsWith("下午吃什么") || msg.StartsWith("下午茶吃什么") || msg.StartsWith("下午茶吃啥")) (category, count) = (下午茶, 2);
+        else if (msg.StartsWith("晚上吃什么") || msg.StartsWith("晚餐吃什么") || msg.StartsWith("晚餐吃啥")) (category, count) = (晚餐, 3);
+        else if (msg.StartsWith("半夜吃什么") || msg.StartsWith("宵夜吃什么")|| msg.StartsWith("宵夜吃啥")) (category, count) = (宵夜, 2);
         else return;
         
-        var food = await RollRandomFood(category, token);
-        
-        await qqService.SendMessageToSomeGroup([message.GroupId], token,
-            new PlainMessage(food.Name));
+        var foods = await RollRandomFood(category, count, token);
+        var name = string.Join("、", foods.Select(f => f.Name));
+
+        await qqService.SendMessageToSomeGroup([message.GroupId], token, new PlainMessage(name));
     }
 }
