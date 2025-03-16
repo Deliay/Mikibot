ffde2b36b95e7251770f299e7a2c4ea6401dee38
ZeroAsh <admin@remiliascarlet.com>
2025-03-16 19:13:55 +0800

    feat: support fetch image information

--- Mikibot.Analyze/Bot/ImageProcessorService.cs
+++ Mikibot.Analyze/Bot/ImageProcessorService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using MathNet.Numerics;
 using MemeFactory.Core.Utilities;
 using Microsoft.Extensions.Logging;
@@ -9,6 +10,7 @@ using Mirai.Net.Data.Messages;
 using Mirai.Net.Data.Messages.Concretes;
 using Mirai.Net.Data.Messages.Receivers;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Gif;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.Formats.Png;
@@ -50,6 +52,42 @@ public class ImageProcessorService(
         return ValueTask.CompletedTask;
     }
 
+    private async IAsyncEnumerable<MessageBase> GetImageInfoAsync(IEnumerable<ImageMessage> messages,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        foreach (var imageMessage in messages)
+        {
+            using var image = await QqService.ReadImageAsync(imageMessage.Url, cancellationToken);
+            var info = image.Width + "*" + image.Height;
+            if (image.Metadata.DecodedImageFormat is { } decoder)
+            {
+                info += $",{decoder.Name} ({decoder.FileExtensions.FirstOrDefault() ?? "未知"})";
+            }
+
+            if (image.Metadata.TryGetGifMetadata(out var gif))
+            {
+                info += "\n-----gif-----\n";
+                info += "重复: " + (gif.RepeatCount == 0 ? "无限" : gif.RepeatCount + "次");
+                info += "帧间隔: " + string.Join(',', image.Frames
+                    .Select(f => f.Metadata.GetGifMetadata().FrameDelay)
+                    .Aggregate(new List<(int delay, int count)>(), (lst, currentDelay) =>
+                    {
+                        if (lst.Count == 0) lst.Add((currentDelay, 1));
+                        else
+                        {
+                            if (lst[^1].delay == currentDelay) lst[^1] = lst[^1] with { count = lst[^1].count + 1 };
+                            else lst.Add((currentDelay, 1));
+                        }
+
+                        return lst;
+                    })
+                    .Select(p => $"{p.delay * 10}ms x {p.count}f"));
+            }
+
+            yield return new PlainMessage(info);
+        }
+    }
+    
     protected override async ValueTask Process(GroupMessageReceiver message, CancellationToken token = default)
     {
         var groupId = message.Sender.Group.Id;
@@ -58,6 +96,22 @@ public class ImageProcessorService(
         Logger.LogInformation("Meme processor, processing: {}", msg);
         if (!msg.StartsWith('/')) return;
 
+        var imageMessages = message.MessageChain
+            .Concat(message.MessageChain.OfType<QuoteMessage>().SelectMany(q => q.Origin))
+            .OfType<ImageMessage>()
+            .ToList();
+
+        if (msg == "/娶")
+        {
+            if (imageMessages.Count == 0) return;
+            var infoMsg = await GetImageInfoAsync(imageMessages, token).ToArrayAsync(token);
+            if (infoMsg.Length == 0)
+            {
+                await QqService.SendMessageToSomeGroup([groupId], token, infoMsg);
+            }
+            return;
+        }
+        
         if (msg.StartsWith("//帮"))
         {
             var helpStr = string.Join(" | ", memeCommandHandler.MemeHelpers
@@ -73,11 +127,6 @@ public class ImageProcessorService(
 
         if (processor is null) return;
 
-        var imageMessages = message.MessageChain
-            .Concat(message.MessageChain.OfType<QuoteMessage>().SelectMany(q => q.Origin))
-            .OfType<ImageMessage>()
-            .ToList();
-        
         if (imageMessages.Count is 0 or > 10)
         {
             Logger.LogInformation("Too many or no image in message chain! Count: {}",
